c8c814623b3d63b3355fa088cb1bcb2540947e06
OKarina <karinkaog@gmail.com>
2023-11-10 21:45:38 +0300

    deb

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include "mainwindow.h"
+#include "QMessageBox"
+#include <windows.h>
+
+bool isDebuggerPresent() {
+    if (IsDebuggerPresent()) {
+        QMessageBox::critical(nullptr, "Ошибка", "Подключен Debugger");
+        return true;
+    }
+    return false;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    if (isDebuggerPresent()){
+        exit(-1);
+    }
     return a.exec();
 }
